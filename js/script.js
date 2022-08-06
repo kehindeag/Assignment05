c2c52e8944c7c4a77e69c36c7cb050c7d9af5457
kehindeag <kenagboola@gmail.com>
2022-08-05 22:57:31 -0700

    Edit the js

--- js/script.js
+++ js/script.js
@@ -21,21 +21,16 @@ output = $("output");
 
 // CONSOLE LOG OR PUT IN PAGE USING innerHTML AND RETURN THE RESULT
 function employeeDetail(id, fname, ext, email, department) {
-  console.log(`ID: ${id}`);
-  console.log(`Full Name: ${fname} `);
-  console.log(`Extension: ${ext}`);
-  console.log(`Email: ${email}`);
-  console.log(`Department: ${department}`);
-  output.innerHTML = `ID: ${id} <br> Full Name: ${fname} <br> Extension: ${ext} <br> Email: ${email} <br> Department: ${department}`;
+  console.log(`ID: ${id.value}`);
+  console.log(`Full Name: ${fname.value} `);
+  console.log(`Extension: ${ext.value}`);
+  console.log(`Email: ${email.value}`);
+  console.log(`Department: ${department.value}`);
+  output.innerHTML = `ID: ${id.value} <br> Full Name: ${fname.value} <br> Extension: ${ext.value} <br> Email: ${email.value} <br> Department: ${department.value}`;
   return;
 }
 
 // GET FORM VALUES ,LOG AND  DISPLAY RESULTS
 submit.addEventListener("click", () => {
-  id = id.value;
-  fname = fname.value;
-  ext = ext.value;
-  email = email.value;
-  department = department.value;
   employeeDetail(id, fname, ext, email, department);
 });
